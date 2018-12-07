656f70b0e83525f868b3f77102e237217fcf25c7
Mateusz Górski <gorski.mateusz@protonmail.ch>
2018-12-07 17:10:25 +0100

    Extract methods

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -4,8 +4,9 @@ import java.nio.channels.Channels
 
 import com.google.protobuf.CodedInputStream
 import jnr.unixsocket.{UnixSocketAddress, UnixSocketChannel}
+import models.Person
 
-object Hello extends Greeting with App {
+object Hello extends App {
   val socketPath = args(0)
   val name = args(1)
   val age = args(2).toInt
@@ -19,19 +20,25 @@ object Hello extends Greeting with App {
     val socketAddr = new UnixSocketAddress(path)
     val channel = UnixSocketChannel.open(socketAddr)
     System.out.println(s"Connected to socket ${channel.getRemoteSocketAddress}.")
+
+    writePerson(channel, person)
+    val receivedPerson = readPerson(channel)
+
+    System.out.println(s"Person received is the same as sent: ${person == receivedPerson}")
+  }
+
+  def readPerson(channel: UnixSocketChannel): Person = {
+    val ir = Channels.newInputStream(channel)
+    val is = CodedInputStream.newInstance(ir)
+    val receivedPerson = person.mergeFrom(is)
+    receivedPerson
+  }
+
+  def writePerson(channel: UnixSocketChannel, person: Person): Unit = {
     val os = Channels.newOutputStream(channel)
     person.writeTo(os)
     System.out.println("Message sent.")
     os.flush()
-
-    val ir = Channels.newInputStream(channel)
-    val is = CodedInputStream.newInstance(ir)
-    val receivedPerson = person.mergeFrom(is)
-    System.out.println(s"Person received is the same as sent: ${person == receivedPerson}")
   }
 
-}
-
-trait Greeting {
-  lazy val greeting: String = "hello"
 }
